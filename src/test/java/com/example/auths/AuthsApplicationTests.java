3abb93f4652733bbdedc4f684ddb16d3aa8762c4
xcv <xcvexec@outlook.com>
2023-01-03 17:12:45 +0800

    init

---
+++ src/test/java/com/example/auths/AuthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.auths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
